87e2dda6b98c48f165ed9fdcd0769310c5db47ba
Anuj Kumar Sharma <anujkumarsharma049@gmail.com>
2026-06-05 14:21:46 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size()==1) {
+            return strs[0];  // so from Now , I will always test my code on the Given Example and also the Edge Cases that is Constraint s.
+        }
+        vector<int>vec;
+        string ans= strs[0];
+        
+
+        for (int i=1; i<strs.size(); i++) {
+            int count=0;
+            for (int j=0; j<min(strs[i].size(), ans.size()); j++) {
+                if (ans[j]!=strs[i][j]) {
+                    break; // i forgot this because what if starting ke not matched and random kahi ke matched 
+                }
+                 else if (ans[j]==strs[i][j]) {
+                    count++;
+                }
+            }
+
+            vec.push_back(count);
+        }
+
+        sort(vec.begin(), vec.end());
+        ans = ans.substr(0, vec[0]); // Returns the prefix of length vec[0]  because the trim function was not there here in c++ .
+        return ans;
+    }
+};  

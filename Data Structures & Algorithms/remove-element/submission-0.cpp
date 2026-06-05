5757ec322a16a513fe63b38915ff07734a7d528d
Anuj Kumar Sharma <anujkumarsharma049@gmail.com>
2026-06-05 18:59:48 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        int k;
+        int count=0;
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i]==val) {
+                nums[i]= 105;
+                count++;
+            }
+        }
+        sort(nums.begin(), nums.end()) ;
+
+        k=nums.size()-count;
+
+        return k;
+
+
+        
+    }
+};
